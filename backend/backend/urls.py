34310ea2305489fddcbcb83a6dd511b17fca0390
Manorama Pradhan <manoramapradhancse2020@gift.edu.in>
2025-12-10 10:11:03 +0530

    Add OpenAPI schema endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title="Django API")
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +15,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
+    path("openapi/", schema_view, name="openapi-schema"),
 ]
